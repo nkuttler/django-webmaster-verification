b65fe098b7b18833914b3f51745cc199edad239c
Nicolas Kuttler <office@nicolaskuttler.com>
2012-02-25 22:42:46 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-webmaster-verification",
+    version = "0.1",
+    author = "nkuttler",
+    author_email = "python@nicolaskuttler.com",
+    description = "Webmaster tools verification for Django",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    url = "http://github.com/nkuttler/django-webmaster-verification",
+    packages=find_packages(),
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ],
+)
