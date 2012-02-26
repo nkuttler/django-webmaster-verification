f09c522abfa550cefaf7c204ef940ad3e1647a01
Nicolas <github@nicolaskuttler.de>
2012-02-26 01:06:37 +0100

    0.1.1 and package the templates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name = "django-webmaster-verification",
-    version = "0.1",
+    version = "0.1.1",
     author = "Nicolas Kuttler",
     author_email = "pypi@nicolaskuttler.com",
     description = "Webmaster tools verification for Django",
@@ -13,6 +13,9 @@ setup(
     license = "BSD",
     url = "http://github.com/nkuttler/django-webmaster-verification",
     packages = ['webmaster_verification'],
+    package_data = {
+        'webmaster_verification': ['templates/webmaster_verification/*', ],
+    },
     classifiers = [
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
