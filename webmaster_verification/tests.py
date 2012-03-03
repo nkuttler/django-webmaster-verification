7935352eb5015ed8d17d1a3ce2da2ec21788daa4
Nicolas Kuttler <office@nicolaskuttler.com>
2012-03-03 15:13:04 +0100

    Use settings and check file contents
    Re-use the codes from the settings files and check the contents of the
    verification files.

--- webmaster_verification/tests.py
+++ webmaster_verification/tests.py
@@ -1,21 +1,33 @@
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+
 
 class WebmasterVerificationTest(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_google_file_access(self):
-        url = '/googleffffffffffffffff.html'
+    def test_google_file_access_and_content(self):
+        code = settings.WEBMASTER_VERIFICATION['google']
+        url = self._get_google_url(code)
         r = self.client.get(url)
         self.assertEqual(
             r.status_code,
             200,
             "Couldn't access %s, got %d" % (url, r.status_code)
         )
+        self.assertRegexpMatches(
+            r.content,
+            '.*%s.*' % code,
+            'Verification code not found in response body',
+        )
 
-    def test_bing_file_access(self):
+    def _get_google_url(self, code):
+        return '/google%s.html' % code
+
+    def test_bing_file_access_and_content(self):
+        code = settings.WEBMASTER_VERIFICATION['bing']
         url = '/BingSiteAuth.xml'
         r = self.client.get(url)
         self.assertEqual(
@@ -28,9 +40,14 @@ class WebmasterVerificationTest(TestCase):
             'text/xml',
             "Got %s content type for robots.txt" % r['Content-Type']
         )
+        self.assertRegexpMatches(
+            r.content,
+            '.*%s.*' % code,
+            'Verification code not found in response body',
+        )
 
     def test_mj_file_access(self):
-        url = '/MJ12_FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF.txt'
+        url = self._get_mj_url(settings.WEBMASTER_VERIFICATION['majestic'])
         r = self.client.get(url)
         self.assertEqual(
             r.status_code,
@@ -42,3 +59,6 @@ class WebmasterVerificationTest(TestCase):
             'text/plain',
             "Got %s content type for robots.txt" % r['Content-Type']
         )
+
+    def _get_mj_url(self, code):
+        return '/MJ12_%s.txt' % code
